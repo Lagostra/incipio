3277abfa0c048ac81076f74a228a8b115b259a03
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-07-18 11:27:36 +0200

    Add simple node backend

---
+++ server.mjs
@@ -0,0 +1,62 @@
+import express from "express";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const config = {
+  redirectUri: process.env.REDIRECT_URI || "http://localhost:3000/callback",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  scope: process.env.SCOPE || "",
+};
+
+const validStates = [];
+
+app.use(
+  express.static("dist", {
+    etag: true,
+    lastModified: true,
+    setHeaders: (res, path) => {
+      const bundleHashRegex = new RegExp("\\.[0-9a-f]{8}\\.");
+      if (path.endsWith(".html")) {
+        // Cacher aldri index.html
+        res.setHeader("Cache-Control", "no-cache");
+      } else if (bundleHashRegex.test(path)) {
+        // Cacher bundle i 5 dager
+        res.setHeader("Cache-Control", "public, max-age=432000");
+      }
+    },
+  })
+);
+
+app.use("/oauth/authorize", (req, res) => {
+  const state = Crypto.getRandomValues(new Uint8Array(16)).toString();
+  validStates.push(state);
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${config.clientId}&redirect_uri=${config.redirectUri}&scope=${config.scope}&state=${state}`
+  );
+});
+
+app.use("/oauth/access_token", async (req, res) => {
+  const { code, state } = req.query;
+  if (!validStates.includes(state)) {
+    res.status(400).send("Invalid state");
+    return;
+  }
+
+  const response = await fetch(`https://github.com/login/oauth/access_token`, {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify({
+      client_id: config.clientId,
+      client_secret: config.clientSecret,
+      code,
+      redirect_uri: config.redirectUri,
+    }),
+  });
+
+  const content = await response.json();
+  res.send({ accessToken: content.access_token });
+});
