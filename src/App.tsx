824e0ddcdd4925d2a46c48b3498d7cdbddef8521
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-07-18 14:31:21 +0200

    Set up Emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { css } from "@emotion/react";
 import { RepositoryList } from "./components/RepositoryList";
 
 export const App = () => {
+  const wrapperStyle = css`
+    display: flex;
+    justify-content: center;
+  `;
+  const contentStyle = css`
+    width: 100%;
+    max-width: 1024px;
+    padding: 15px;
+  `;
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main css={wrapperStyle}>
+      <div css={contentStyle}>
+        <h1>Hello World!</h1>
 
-      <RepositoryList />
+        <RepositoryList />
+      </div>
     </main>
   );
 };
